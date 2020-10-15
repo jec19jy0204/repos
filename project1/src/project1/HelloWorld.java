8a0862e8dfa822d770fa94f836661eb4dc86ffac
19jy0204 <19jy0204@PC03.jynet2.jec.ac.jp>
2020-10-15 10:20:38 +0900

    mod 乱数を10から20までカウントダウン

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int num = (int)(Math.random()*10)+10;
-		for(int i = 1 ; i <= num ; i++) {
+		for(int i = num ; i >=0  ; i--) {
 			System.out.println("HelloWorld " +i);
 		}
 	}
