9126883e877febaee772a64ec9030cbf9ea8b912
19jy0204 <19jy0204@PC03.jynet2.jec.ac.jp>
2020-10-15 10:13:35 +0900

    Revert "mod 乱数を10回から20回に増やして表示する"
    This reverts commit 9efd0d8ec52d69bd413c480709e09a1cf4cb314e.

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int num = (int)(Math.random()*10)+10;
+		int num = (int)(Math.random()*10)+1;
 		for(int i = 1 ; i <= num ; i++) {
 			System.out.println("HelloWorld " +i);
 		}
