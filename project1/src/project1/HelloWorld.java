6791d05234561c9bbe8dd7415c2bd1778ae79f56
19jy0204 <19jy0204@PC03.jynet2.jec.ac.jp>
2020-10-15 10:01:06 +0900

    mod for文で5回表示するように変更

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -4,9 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.print("HelloWorld");
-		System.out.print("HelloWorld チェック");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld チェック");
+		for(int i = 0 ; i < 5 ; i++ ) {
+			System.out.println("HelloWorld" + i);
 
+		}
 	}
 
 }
